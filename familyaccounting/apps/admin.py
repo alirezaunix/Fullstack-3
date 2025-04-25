b109af13a665e8bd3c443e3f1f9f00e2fe147951
alirezaunix <alirezaunix@gmail.com>
2025-04-25 22:02:25 +0330

    #2

--- familyaccounting/apps/admin.py
+++ familyaccounting/apps/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Family,IOMoney
 # Register your models here.
+
+@admin.register(Family)
+class FamilyAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(IOMoney)
+class IOMoneyAdmin(admin.ModelAdmin):
+    pass
--- familyaccounting/apps/models.py
+++ familyaccounting/apps/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Family(models.Model):
+    id=models.AutoField(primary_key=True)
+    fname=models.CharField(max_length=250)
+    tel=models.CharField(max_length=20)
+    
+
+
+class IOMoney(models.Model):
+    id=models.AutoField(primary_key=True)
+    familymember=models.ForeignKey(Family,on_delete=models.CASCADE)
+    money=models.FloatField(default=0)
+    desc=models.CharField(max_length=500)
+    iodate=models.DateField()
+    addeddate=models.DateField(auto_created=True)
+    
